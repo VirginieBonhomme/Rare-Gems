65b4683b516f1d85d85c4f58e3a37e4afffa38a6
Virginie Bonhomme <virginiebonhomme@Virginies-MBP.home>
2022-03-02 09:53:58 -0500

    deployment

--- client/src/Screens/Sneakers.jsx
+++ client/src/Screens/Sneakers.jsx
@@ -5,7 +5,7 @@ export default function Sneakers(props) {
   return (
     <div class="flex flex-wrap justify-center items-center" >
       {
-        props.sneakers.map(sneaker => (
+        props.sneakers?.map(sneaker => (
           <div class="w-60 p-2 m-4 bg-white rounded-xl transform transition-all hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl">
             <Link
               key={sneaker.id} to={`/sneakers/${sneaker.id}`}>
